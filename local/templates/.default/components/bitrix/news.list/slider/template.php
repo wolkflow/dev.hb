1f4b70b81bdf0420dd3cb36e513bf44163cf3dbf
root <root@vps-1045602-16500.host4g.ru>
2016-11-05 01:53:04 +0300

    CSS

--- local/templates/.default/components/bitrix/news.list/slider/template.php
+++ local/templates/.default/components/bitrix/news.list/slider/template.php
@@ -2,30 +2,8 @@
 
 <? $this->setFrameMode(true); ?>
 
-<? /*<div class="sliderArea">
-    <div id="sliderDots"></div>
-    <div class="slider" id="homeSlider">
-        <? foreach ($arResult['ITEMS'] as $item) { ?>
-            <div>
-                <div class="container">
-                    <div class="sliderText">
-                        <div class="sliderTitle">
-                            <?= $item['NAME'] ?>
-                        </div>
-                        <p>
-                            <?= $item['PREVIEW_TEXT'] ?>
-                        </p>
-                    </div>
-                    <div class="sliderImage">
-                        <img src="/i.php?src=<?= $item['PREVIEW_PICTURE']['SRC'] ?>&w=856&h=803" />
-                    </div>
-                </div>
-            </div>
-        <? } ?>
-    </div>
-</div>*/ ?>
 
-<div id="carousel" class="carousel slide">
+<div id="carousel" class="carousel slide hei7">
     <div class="container1">
         <ol class="carousel-indicators">
             <? $first = true ?>
