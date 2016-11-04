23633d18e61489cb1923aca767824a96c3380bf0
root <root@vps-1045602-16500.host4g.ru>
2016-11-04 21:13:12 +0300

    CSS.

--- local/templates/.default/components/bitrix/news.list/slider/template.php
+++ local/templates/.default/components/bitrix/news.list/slider/template.php
@@ -28,42 +28,66 @@
 <div id="carousel" class="carousel slide">
   <!-- Индикаторы слайдов -->
   <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel" data-slide-to="1"></li>
+    <li data-target="#carousel" data-slide-to="2"></li>
   </ol>
 
   <!-- Слайды -->
   <div class="carousel-inner">
         <div class="item active">
-          <img src="http://educationcareerarticles.com/wp-content/uploads/2014/07/Chef6.jpg" alt=" ">
-          <div class="carousel-caption">
-                <h3>Популяризация здоровой и полезной еды</h3>
-                <p>В бешеном ритме современного человека зачастую не остается времени на приготовление хорошей и сбалансированной еды.</p>
-          </div>
-        </div>
-        <div class="item">
-            <img src="http://meownauts.com/wp-content/uploads/2015/11/mgid-uma-image-mtv.com-10719123.jpg" alt=" ">
-            <div class="carousel-caption">
-                <h3>Популяризация здоровой и полезной еды 2</h3>
-                <p>В бешеном ритме современного человека зачастую не остается времени на приготовление хорошей и сбалансированной еды.</p>
+            <img src="http://educationcareerarticles.com/wp-content/uploads/2014/07/Chef6.jpg" alt="">
+            <div class="carousel-caption container hei8">
+                <div class="row">
+                    <div class="col-sm-7 col-md-7 col-lg-7">
+                        <div class="carousel_background1">
+                        </div>
+                        <div class="carousel_content1">
+                            <h3>Популяризация здоровой и полезной еды</h3>
+                            <p>В бешеном ритме современного человека зачастую не остается времени на приготовление хорошей и сбалансированной еды.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="item">
-            <img src="http://mypizzagrill.ru/wp-content/uploads/2014/12/Chef9.jpg" alt=" ">
-            <div class="carousel-caption">
-                <h3>Популяризация здоровой и полезной еды 3</h3>
-                <p>В бешеном ритме современного человека зачастую не остается времени на приготовление хорошей и сбалансированной еды.</p>
+            <img src="http://meownauts.com/wp-content/uploads/2015/11/mgid-uma-image-mtv.com-10719123.jpg" alt="">
+            <div class="carousel-caption container hei8">
+                <div class="row">
+                    <div class="col-sm-7 col-md-7 col-lg-7">
+                        <div class="carousel_background1">
+                        </div>
+                        <div class="carousel_content1">
+                            <h3>Популяризация здоровой и полезной еды</h3>
+                            <p>В бешеном ритме современного человека зачастую не остается времени на приготовление хорошей и сбалансированной еды.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="http://mypizzagrill.ru/wp-content/uploads/2014/12/Chef9.jpg" alt="">
+            <div class="carousel-caption container hei8">
+                <div class="row">
+                    <div class="col-sm-7 col-md-7 col-lg-7">
+                        <div class="carousel_background1">
+                        </div>
+                        <div class="carousel_content1">
+                            <h3>Популяризация здоровой и полезной еды</h3>
+                            <p>В бешеном ритме современного человека зачастую не остается времени на приготовление хорошей и сбалансированной еды.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
   </div>
 
   <!-- Стрелки переключения слайдов -->
-  <a href="#carousel" class="left carousel-control" data-slide="prev">
+  <!--<a href="#carousel" class="left carousel-control" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
   </a>
   <a href="#carousel" class="right carousel-control" data-slide="next">
         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-  </a>
+  </a>-->
 </div>
 
