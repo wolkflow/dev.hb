c5bffbe1b813b5b52c17aef2181c608fd648f909
root <root@vps-1045602-16500.host4g.ru>
2016-11-05 00:57:50 +0300

    CSS

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -1,100 +1,89 @@
             <footer class="siteFooter">
 				<div class="bg2">
 				</div>
-                <div class="container">
-					<div class="row">
-						<div class="col-lg-12 col-md-12 col-sm-12">
-							<div class="row">
-								<div class="col-lg-12 col-md-12 col-sm-12 bg1">	
-									<!--<div class="bg1">
-									</div>-->
-									<div class="row">
-										
-										<div class="col-lg-9 col-md-9 col-sm-9 foo1">
-											<div class="menu footerMenu foo2">
-												<ul>
-													<li><a href="/about/">о нас</a></li>
-													<li><a href="/#programs">программы питания</a></li>
-													<li><a href="/catalog/">holymarket</a></li>
-													<li><a href="#" class="popup-opener" data-popup="#basket-popup">корзина</a></li>
-												</ul>
-											</div>
-										</div>
-										<div class="col-lg-3 col-md-3 col-sm-3">
-											<div class="footer-contacts">
-												<div class="footer-contacts__tel">
-													<?  // Телефон.
-														$APPLICATION->IncludeComponent('bitrix:main.include', '', array(
-															'AREA_FILE_SHOW' => 'file',
-															'PATH' => SITE_TEMPLATE_PATH.'/include/data/phone.php',
-															'EDIT_TEMPLATE' => 'html',
-														));
-													?>
-												</div>
-												<div class="footer-contacts__soc hidden-xs">
-													<a href="#">
-														<i class="icon-12 icon-inst"></i>
-													</a>
-													<a href="#">
-														<i class="icon-12 icon-fb"></i>
-													</a>
-												</div>
-											</div>
-										</div>
-									</div>
-									<div class="row footer-top">
-										<div class="col-lg-3 col-md-3 col-sm-3 foo1">
-											<div class="footer-info__copyright foo2">
-												<span>
-													<?  // Copyright.
-														$APPLICATION->IncludeComponent('bitrix:main.include', '', array(
-															'AREA_FILE_SHOW' => 'file',
-															'PATH' => SITE_TEMPLATE_PATH.'/include/data/copyright.php',
-															'EDIT_TEMPLATE' => 'html',
-														));
-													?>
-												</span>
-												<span>
-													<?  // Разработка.
-														$APPLICATION->IncludeComponent('bitrix:main.include', '', array(
-															'AREA_FILE_SHOW' => 'file',
-															'PATH' => SITE_TEMPLATE_PATH.'/include/data/development.php',
-															'EDIT_TEMPLATE' => 'html',
-														));
-													?>
-												</span>
-											</div>
-										</div>
-										<div class="col-lg-6 col-md-6 col-sm-6">
-											<div class="footer-info__copyright footer-info2">
-												<span>
-												<?  // Оферта.
-													$APPLICATION->IncludeComponent('bitrix:main.include', '', array(
-														'AREA_FILE_SHOW' => 'file',
-														'PATH' => SITE_TEMPLATE_PATH.'/include/data/offer.php',
-														'EDIT_TEMPLATE' => 'html',
-													));
-												?>
-												</span>
-											</div>
-										</div>
-										<div class="col-lg-3 col-md-3 col-sm-3">
-											<div class="footer-info__copyright footer-info3">
-												<span>
-												<?  // E-mail.
-													$APPLICATION->IncludeComponent('bitrix:main.include', '', array(
-														'AREA_FILE_SHOW' => 'file',
-														'PATH' => SITE_TEMPLATE_PATH.'/include/data/email.php',
-														'EDIT_TEMPLATE' => 'html',
-													));
-												?>
-												</span>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+                <div class="container2 hei9 bg1">
+					<div class="row">                          
+                        <div class="col-lg-9 col-md-9 col-sm-9 foo1">
+                            <div class="menu footerMenu">
+                                <ul>
+                                    <li><a href="/about/">о нас</a></li>
+                                    <li><a href="/#programs">программы питания</a></li>
+                                    <li><a href="/catalog/">holymarket</a></li>
+                                    <li><a href="#" class="popup-opener" data-popup="#basket-popup">корзина</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-3">
+                            <div class="footer-contacts">
+                                <div class="footer-contacts__tel">
+                                    <?  // Телефон.
+                                        $APPLICATION->IncludeComponent('bitrix:main.include', '', array(
+                                            'AREA_FILE_SHOW' => 'file',
+                                            'PATH' => SITE_TEMPLATE_PATH.'/include/data/phone.php',
+                                            'EDIT_TEMPLATE' => 'html',
+                                        ));
+                                    ?>
+                                </div>
+                                <div class="footer-contacts__soc hidden-xs">
+                                    <a href="#">
+                                        <i class="icon-12 icon-inst"></i>
+                                    </a>
+                                    <a href="#">
+                                        <i class="icon-12 icon-fb"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row footer-top">
+                        <div class="col-lg-3 col-md-3 col-sm-3 foo1">
+                            <div class="footer-info__copyright">
+                                <span>
+                                    <?  // Copyright.
+                                        $APPLICATION->IncludeComponent('bitrix:main.include', '', array(
+                                            'AREA_FILE_SHOW' => 'file',
+                                            'PATH' => SITE_TEMPLATE_PATH.'/include/data/copyright.php',
+                                            'EDIT_TEMPLATE' => 'html',
+                                        ));
+                                    ?>
+                                </span>
+                                <span>
+                                    <?  // Разработка.
+                                        $APPLICATION->IncludeComponent('bitrix:main.include', '', array(
+                                            'AREA_FILE_SHOW' => 'file',
+                                            'PATH' => SITE_TEMPLATE_PATH.'/include/data/development.php',
+                                            'EDIT_TEMPLATE' => 'html',
+                                        ));
+                                    ?>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="footer-info__copyright footer-info2">
+                                <span>
+                                <?  // Оферта.
+                                    $APPLICATION->IncludeComponent('bitrix:main.include', '', array(
+                                        'AREA_FILE_SHOW' => 'file',
+                                        'PATH' => SITE_TEMPLATE_PATH.'/include/data/offer.php',
+                                        'EDIT_TEMPLATE' => 'html',
+                                    ));
+                                ?>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-3">
+                            <div class="footer-info__copyright footer-info3">
+                                <span>
+                                <?  // E-mail.
+                                    $APPLICATION->IncludeComponent('bitrix:main.include', '', array(
+                                        'AREA_FILE_SHOW' => 'file',
+                                        'PATH' => SITE_TEMPLATE_PATH.'/include/data/email.php',
+                                        'EDIT_TEMPLATE' => 'html',
+                                    ));
+                                ?>
+                                </span>
+                            </div>
+                        </div>
 					</div>
 				</div>
 						
