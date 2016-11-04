97083eb98c8586c3691ffb426118381973a40d5d
root <root@vps-1045602-16500.host4g.ru>
2016-11-05 01:29:23 +0300

    CSS

--- local/templates/.default/components/bitrix/news.detail/whoweare/template.php
+++ local/templates/.default/components/bitrix/news.detail/whoweare/template.php
@@ -1,8 +1,7 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
 <? $this->setFrameMode(true); ?>
-
-
+<div class="poz1">
     <div class="container main-unit hei6">
         <div class="row">
             <div class="col-sm-7">
@@ -13,15 +12,18 @@
                     </p>
                 </div>
             </div>
-            <div class="col-sm-5">
-                <div class="main-who_images">
-                    <? if (!empty($arResult['PREVIEW_PICTURE']['SRC'])) { ?>
-                        <img class="main-who__smallimg" src="/i.php?src=<?= $arResult['PREVIEW_PICTURE']['SRC'] ?>&w=220&h=143" />
-                    <? } ?>
-                    <? if (!empty($arResult['DETAIL_PICTURE']['SRC'])) { ?>
-                        <img class="main-who__bigimg" src="/i.php?src=<?= $arResult['DETAIL_PICTURE']['SRC'] ?>&w=320&h=479" />
-                    <? } ?>
-                </div>
-            </div>
         </div>
     </div>
+    <div class="poz2">
+    <div class="main-who_images">
+        
+        <? if (!empty($arResult['PREVIEW_PICTURE']['SRC'])) { ?>
+            <img class="main-who__smallimg" src="/i.php?src=<?= $arResult['PREVIEW_PICTURE']['SRC'] ?>&w=220&h=143" />
+        <? } ?>
+        <? if (!empty($arResult['DETAIL_PICTURE']['SRC'])) { ?>
+            <img class="main-who__bigimg" src="/i.php?src=<?= $arResult['DETAIL_PICTURE']['SRC'] ?>&w=320&h=479" />
+        <? } ?>
+        </div>
+    </div>
+</div>
+    
