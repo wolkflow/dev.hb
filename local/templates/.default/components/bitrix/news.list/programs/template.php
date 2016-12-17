18151dfe0f082b366599e836b29c7f5d29c0f07b
root <root@vps-1045602-16500.host4g.ru>
2016-12-17 20:16:02 +0300

    Programs icons.

--- local/templates/.default/components/bitrix/news.list/programs/template.php
+++ local/templates/.default/components/bitrix/news.list/programs/template.php
@@ -16,8 +16,9 @@
                     <? $link = ($item['PROPERTIES']['SOON']['VALUE'] == 'Y') ? ('javascript:void(0)') : ($item['DETAIL_PAGE_URL']) ?>
                     <a href="<?= $link ?>" class="main-programs__block col-sm-4 col-md-4 col-lg-4 col-xs-12 <?= ($item['PROPERTIES']['SOON']['VALUE'] == 'Y') ? ('disable') : ('') ?>">
                         <svg aria-hidden="true">
-                            <use xlink:href="<?= SITE_TEMPLATE_PATH ?>/images/icons.svg#<?= $item['PROPERTIES']['CSS']['VALUE'] ?>"></use>
+                            <use xlink:href="<?= SITE_TEMPLATE_PATH ?>/images/111.svg"></use>
                         </svg>
+
                         <?= $item['NAME'] ?>
                         <? if ($item['PROPERTIES']['SOON']['VALUE'] == 'Y') { ?>
                             <br/><span class="soon">(скоро в продаже)</span>
